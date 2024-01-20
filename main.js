e3abb9317ff94ebaeda48f513113b9fd3c6fc9f6
jimmy20180130 <68285469+jimmy20180130@users.noreply.github.com>
2024-01-20 10:49:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 let config = JSON.parse(fs.readFileSync(`${process.cwd()}/config.json`, 'utf8'))
 let bot;
 let continue_list = Object.keys(config.warps);
-
+let run = true
 let botargs = {
     username: config.username,
     host: config.host,
@@ -33,6 +33,7 @@ async function sign(warp) {
         const other_already_signed_pattern = /\[(.*?) -> 您\] (.*)/;
         const bot_not_online_pattern = /\[系統\] 無法取得 (.*?) 的玩家資料，您打錯ID了嗎\?/;
         const konjac_success_pattern = /\[(.*?) -> 您\] \[\d{2}:\d{2}:\d{2}\]: (.*?)/;
+        const system_pattern = /^\[系統\]/;
 
         try {
             const msg = await Promise.race([
@@ -42,6 +43,7 @@ async function sign(warp) {
                 bot.awaitMessage(konjac_wait_pattern),
                 bot.awaitMessage(other_already_signed_pattern),
                 bot.awaitMessage(bot_not_online_pattern),
+                bot.awaitMessage(system_pattern),
                 new Promise((resolve) => setTimeout(() => resolve('Timeout'), 20000))
             ]);
 
@@ -52,11 +54,12 @@ async function sign(warp) {
                 console.log(`成功: 已成功簽到！\n領取數量: ${receivedAmount} 個\n目前數量: ${currentAmount} 個`)
                 const konjac_success_msg = await Promise.race([bot.awaitMessage(konjac_success_pattern), new Promise((resolve) => setTimeout(() => resolve('Timeout'), 20000))])
 
-                if (konjac_success_msg == 'Timeout') return;
-                const regex = /https:\/\/dice\.patyhank\.net\S*/g;
-                const matches = msg.match(regex);
-                if (matches != null) {
-                    console.log(`資訊: 請完成防人機驗證\n說明: 請完成防人機驗證以繼續至下一個傳點簽到，十秒鐘後繼續下個傳點\n連結: ${matches[0]}`);
+                if (konjac_success_msg != 'Timeout') {
+                    const regex = /https:\/\/dice\.patyhank\.net\S*/g;
+                    const matches = konjac_success_msg.match(regex);
+                    if (matches != null) {
+                        console.log(`資訊: 請完成防人機驗證\n說明: 請完成防人機驗證以繼續至下一個傳點簽到，十秒鐘後繼續下個傳點\n連結: ${matches[0]}`);
+                    }
                 }
                 continue_list.shift();
                 await new Promise(resolve => setTimeout(resolve, 10000));
@@ -74,6 +77,8 @@ async function sign(warp) {
             } else if (bot_not_online_pattern.test(msg)) {
                 console.log(`錯誤: 機器人不在線上\nBot ID: ${config.warps[warp]}`);
                 continue_list.shift()
+            } else if (system_pattern.test(msg)) {
+                console.log(`錯誤: 系統訊息\n訊息: ${msg}\n此傳點將會在所有簽到處理完後重新嘗試簽到`);
             } else if (msg === '機器人沒回應或發生了意外的錯誤') {
                 console.log('錯誤: 機器人沒回應或發生意外的錯誤\n說明: 此傳點將會在所有簽到處理完後重新嘗試簽到')
             } else {
@@ -100,10 +105,12 @@ async function sign(warp) {
 async function auto_sign() {
     if (continue_list.length == 0) {continue_list = Object.keys(config.warps)}
     for (const warp of continue_list) {
+        if (run == false) {return}
         await sign(warp);
     }
     while (continue_list.length > 0) {
         for (item of continue_list) {
+            if (run == false) {return}
             await sign(item);
             continue_list.shift();
         }
@@ -118,13 +125,17 @@ async function connect() {
         console.log('資訊: 已載入世界，五秒後開始簽到');
         await new Promise(resolve => setTimeout(resolve, 5000));
         await auto_sign();
+        run = true
     });
 
     bot.once('login', async () => {
         console.log('資訊: 已登入伺服器');
     });
 
-    bot.on('error', err => console.log('錯誤: 發生不可預期的錯誤\n', err));
+    bot.on('error', (err) =>{
+        console.log('錯誤: 發生不可預期的錯誤\n', err)
+        run = false
+    });
     bot.on('end', async () => {
         console.log('資訊: 已斷線，將於五秒鐘後重新連線，之前的進度不會受影響');
         await new Promise(resolve => setTimeout(resolve, 5000));
