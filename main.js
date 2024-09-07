6e1d9e8ed30a293a3c69bb5ecc5029df68184352
XiaoXi_TW <68285469+jimmy20180130@users.noreply.github.com>
2024-09-07 11:44:38 +0800

    更新檔案

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let run = true
 let botargs = {
     username: config.username,
     host: config.host,
-    port: config.port,
-    version: config.version,
-    auth: config.auth,
+    port: 25565,
+    version: '1.20.1',
+    auth: 'microsoft',
     keepAlive: true,
     checkTimeoutInterval: 120 * 1000
 }
@@ -29,12 +29,14 @@ async function sign(warp) {
         }
 
         const sign_in_success_pattern = /^(\[系統\] 您收到了 .*\(目前擁有 .*\))$/;
-        const ddddo_already_signed_pattern = /\[(.*?) -> 您\] 今日已重複簽到(\d{4}\/\d{2}\/\d{2})/;
+        const curry_already_signed_pattern = /\[(.*?) -> 您\] 今日已經簽到過了，請明天再來/;
+        const ddddo_already_signed_pattern = /\[(.*?) -> 您\] 今日已簽到，請明日再領取/;
         const konjac_already_signed_pattern = /\[(.*?) -> 您\] 您已領取您的身分組獎勵，請明天再來領取 已領身分組獎勵: \[(.*?)\]/;
         const konjac_wait_pattern = /\[(.*?) -> 您\] 執行這項操作還需要等待 (.*?)/;
         const other_already_signed_pattern = /\[(.*?) -> 您\] (.*)/;
         const bot_not_online_pattern = /\[系統\] 無法取得 (.*?) 的玩家資料，您打錯ID了嗎\?/;
         const konjac_success_pattern = /\[(.*?) -> 您\] \[\d{2}:\d{2}:\d{2}\]: (.*?)/;
+        const ddddo_success_pattern = /\[(.*?) -> 您\] \[\d{2}:\d{2}:\d{2}\] 成功領取(.*?)/;
         const system_pattern = /^\[系統\]/;
 
         try {
@@ -46,36 +48,38 @@ async function sign(warp) {
                 bot.awaitMessage(other_already_signed_pattern),
                 bot.awaitMessage(bot_not_online_pattern),
                 bot.awaitMessage(system_pattern),
+                bot.awaitMessage(curry_already_signed_pattern),
+                bot.awaitMessage(ddddo_success_pattern),
                 new Promise((resolve) => setTimeout(() => resolve('Timeout'), 20000))
             ]);
 
-            if (sign_in_success_pattern.test(msg)) {
+            if (sign_in_success_pattern.test(msg) || ddddo_success_pattern.test(msg) || konjac_success_pattern.test(msg)) {
                 var combinedAmountMatch = msg.match(/您收到了.*?(\d{1,3}(,\d{3})*?) 綠寶石.*?目前擁有 (\d{1,3}(,\d{3})*?) 綠寶石/);
                 var receivedAmount = combinedAmountMatch ? parseInt(combinedAmountMatch[1].replace(/,/g, '')) : 0;
                 var currentAmount = combinedAmountMatch ? parseInt(combinedAmountMatch[3].replace(/,/g, '')) : 0;
                 console.log(`成功: 已成功簽到！\n領取數量: ${receivedAmount} 個\n目前數量: ${currentAmount} 個`)
-                const konjac_success_msg = await Promise.race([bot.awaitMessage(konjac_success_pattern), new Promise((resolve) => setTimeout(() => resolve('Timeout'), 20000))])
 
-                if (konjac_success_msg != 'Timeout') {
-                    const regex = /https:\/\/dice\.patyhank\.net\S*/g;
-                    const matches = konjac_success_msg.match(regex);
-                    if (matches != null) {
-                        console.log(`資訊: 請完成防人機驗證\n說明: 請完成防人機驗證以繼續至下一個傳點簽到，十秒鐘後繼續下個傳點\n連結: ${matches[0]}`);
-                        await new Promise(resolve => setTimeout(resolve, 10000));
-                    }
-                }
+                // //以前的防人機驗證
+                // const konjac_success_msg = await Promise.race([bot.awaitMessage(konjac_success_pattern), new Promise((resolve) => setTimeout(() => resolve('Timeout'), 20000))])
+
+                // if (konjac_success_msg != 'Timeout') {
+                //     const regex = /https:\/\/dice\.patyhank\.net\S*/g;
+                //     const matches = konjac_success_msg.match(regex);
+                //     if (matches != null) {
+                //         console.log(`資訊: 請完成防人機驗證\n說明: 請完成防人機驗證以繼續至下一個傳點簽到，十秒鐘後繼續下個傳點\n連結: ${matches[0]}`);
+                //         await new Promise(resolve => setTimeout(resolve, 10000));
+                //     }
+                // }
                 continue_list.shift();
-            } else if (ddddo_already_signed_pattern.test(msg)) {
-                console.log(`錯誤: 今日您已經簽到過了\nBot ID: ${config.warps[warp]}\n最後簽到時間: ${ddddo_already_signed_pattern.exec(msg)[2]}`);
-                continue_list.shift();
-            } else if (konjac_already_signed_pattern.test(msg)) {
+            } else if (konjac_already_signed_pattern.test(msg) || curry_already_signed_pattern.test(msg) || ddddo_already_signed_pattern.test(msg)) {
                 console.log(`錯誤: 今日您已經簽到過了\nBot ID: ${config.warps[warp]}`);
                 continue_list.shift();
             } else if (konjac_wait_pattern.test(msg)) {
-                const regex = /https:\/\/dice\.patyhank\.net\S*/g;
-                const matches = msg.match(regex);
-                console.log(`錯誤: 防人機驗證未完成\n說明: 請先完成防人機驗證以重新簽到，此傳點將會在所有簽到處理完後重新嘗試簽到\n連結: ${matches[0]}`);
-                await new Promise(resolve => setTimeout(resolve, 15000));
+                // 以前的防人機驗證
+                // const regex = /https:\/\/dice\.patyhank\.net\S*/g;
+                // const matches = msg.match(regex);
+                // console.log(`錯誤: 防人機驗證未完成\n說明: 請先完成防人機驗證以重新簽到，此傳點將會在所有簽到處理完後重新嘗試簽到\n連結: ${matches[0]}`);
+                // await new Promise(resolve => setTimeout(resolve, 15000));
             } else if (bot_not_online_pattern.test(msg)) {
                 console.log(`錯誤: 機器人不在線上\nBot ID: ${config.warps[warp]}`);
                 continue_list.shift()
--- start.js
+++ start.js
@@ -1,9 +1,9 @@
-const { spawn } = require('child_process');
+const { fork } = require('child_process');
 const readline = require('readline')
 
 let appProcess = undefined;
 
-console.log('[INFO] 正在開始執行由 Jimmy 開發的 [廢土鐵原礦機器人]');
+console.log('[INFO] 正在開始執行由 Jimmy 開發的 [廢土掛機機器人]');
 
 let rl = readline.createInterface({
     input: process.stdin,
@@ -15,25 +15,22 @@ rl.on('line', async function (line) {
 });
 
 function startApp() {
-    appProcess = spawn('node', ['main.js']);
+    appProcess = fork(__dirname + '/main.js', [], { silent: true })
 
     appProcess.stdout.on('data', (data) => {
-        console.log(`${String(data).replace(/\n$/, '')}`);
+        console.log(`${String(data).replaceAll('\n', '')}`);
     });
 
     appProcess.stderr.on('data', (data) => {
-        console.log(`[ERROR] 發現以下錯誤 ${data}`);
+        console.error(`[ERROR] 發現以下錯誤 ${data} ，正在重新開啟中...`);
     });
 
+    appProcess.on('error', (err) => {
+        console.log(`[ERROR] ${err}`)
+    })
+
     appProcess.on('close', (code) => {
-        if (code == 135) {
-            console.log(`[INFO] 機器人已關閉`)
-            process.kill()
-        } else if (code == 246) {
-            console.log(`[INFO] 機器人正在重新啟動中...`)
-        } else {
-            console.log(`[ERROR] 程式回傳錯誤碼 ${code} ，正在重新啟動中...`);
-        }
+        console.log(`[ERROR] 程式回傳錯誤碼 ${code} ，正在重新啟動中...`);
         appProcess = undefined
         startApp();
     });
